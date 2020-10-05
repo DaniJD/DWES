d4c0e6c6f2b0ce6258bcda60b55c4a3ea815385b
DaniJD <alujod208@ieselcaminas.org>
2020-10-05 12:56:43 +0200

    Primer commit

---
+++ 4.2.6.php
@@ -0,0 +1,19 @@
+<?php
+$a = 3;
+$b = 7;
+$cadena = 'AB';
+echo "Sean a = $a,b = $b y cadena = '$cadena'<br>";
+$c = --$a + b++ = $c<br>Con b = $b y a = $a<br>";
+++$cadena;
+echo "++cadena = '$cadena'";
+
+?><?php
+$a = 3;
+$b = 7;
+$cadena = 'AB';
+echo "Sean a = $a,b = $b y cadena = '$cadena'<br>";
+$c = --$a + b++ = $c<br>Con b = $b y a = $a<br>";
+++$cadena;
+echo "++cadena = '$cadena'";
+
+?>
